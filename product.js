612470dcb02d55a097cb155b12e75d2da3f3b272
Krins <krinskumar@gmail.com>
2023-04-29 00:20:21 -0400

    added-product.js

---
+++ product.js
@@ -0,0 +1,16 @@
+//queries all the product and categories from the database
+// and saves it in a variable
+
+/*
+Following function:
+1. Queries all the products and categories from the database and save it in a variable
+2. get all products
+3. get all categories
+4. get all products in a category
+5. get a product by id
+6. get a category by id
+7. get all products in a cart
+8. get a cart by id
+9. get all carts
+10. get all carts by user
+*/
