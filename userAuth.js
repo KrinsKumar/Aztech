0c26b2b6f349f35459066b51737eaa39cf46da3e
Krins <krinskumar@gmail.com>
2023-05-01 01:11:35 -0400

    added-userFunction

--- userAuth.js
+++ userAuth.js
@@ -1,1 +1,36 @@
-//handles creating and validation of the user
+let mongoose = require('mongoose');
+let database = require('./database.js');
+
+let Userdb;
+
+exports.initialize = function () {
+    return new Promise(function (resolve, reject) {
+        let db = mongoose.createConnection(process.env.MONGO_CONNECTION_STRING);
+
+        db.on('error', (err) => {
+            reject(err); // reject the promise with the provided error
+        });
+        db.once('open', () => {
+            user = db.model("user", database.userSchema);
+            resolve();
+        });
+    });
+};
+
+exports.registerUser = function (data) {
+    return new Promise((resolve, reject) => {
+        //if the password do not match or the data is empty
+        if ((data.password != data.password2) || !data) {
+            reject("Passwords do not match");
+        }
+
+        let newUser = new Userdb(data);
+        newUser.save((err) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(`new user: ${newUser.userName} successfully registered`);
+            }
+        });
+    });
+};
